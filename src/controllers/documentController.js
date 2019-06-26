2c7ca669800ddbbfcf711bdcc1c0f6055045a59c
alpaca623 <alpaca623@gmail.com>
2019-06-26 23:34:22 +0900

    updating : document crud function - create, read, all document

--- src/controllers/documentController.js
+++ src/controllers/documentController.js
@@ -1,19 +1,42 @@
-export const homeDocument = (req, res) => {
-    res.send("document home");
-}
+import Document from "../models/documentSchema";
 
-export const readDocument = (req, res) => {
-    res.send('read document');
-}
+export const homeDocument = async (req, res) => {
+  try {
+    const documents = await Document.find();
+    res.send(documents);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export const readDocument = async (req, res) => {
+  // res.send("read document");
+  const { _id } = req.body;
+  console.log(res.body);
+  try {
+    const document = await Document.findById(_id);
+    res.send(document);
+  } catch (e) {
+    console.log(e);
+  }
+};
 
 export const createDocument = (req, res) => {
-    res.send('create document');
-}
+  console.log("create document : ", req.body);
+  const { title, content } = req.body;
+  try {
+    Document.create({ title, content }, o => console.log(o));
+    res.send("✅ complete create document");
+  } catch (e) {
+    console.error(e);
+    res.send("❌ complete fail document");
+  }
+};
 
 export const updateDocument = (req, res) => {
-    res.send('update document');
-}
+  res.send("update document");
+};
 
 export const deleteDocument = (req, res) => {
-    res.send('delete document');
-}
+  res.send("delete document");
+};
